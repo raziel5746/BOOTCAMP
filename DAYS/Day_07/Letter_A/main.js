ca53729843ef1d80f33a6740eddde9b71f19ed0c
Raziel <raziel5746@gmail.com>
2019-11-09 21:30:57 +0200

    letter A updated

--- DAYS/Day_07/Letter_A/main.js
+++ DAYS/Day_07/Letter_A/main.js
@@ -1,28 +1,34 @@
-let letterHeight = parseInt(prompt("Define the letter's height by typing a number:"));
-let letterCenter = letterHeight - 1;
+let letterHeight = 0;
+let letterCenter = 0;
 let rows = [];
-let centerRow = Math.ceil(letterHeight / 2);
+let centerRow;
 
 
-for (let i = 0; i < letterHeight; i++) {
-    rows[i] = [];
-    for (let j = 0; j < letterHeight + i; j++) {
-        rows[i].push(" ");
+function drawLetterA() {
+    letterHeight = parseInt(prompt("Define the letter's height by typing a number:"));
+    rows = []
+    letterCenter = letterHeight - 1;
+    centerRow = Math.ceil(letterHeight / 2);
+
+    for (let i = 0; i < letterHeight; i++) {
+        rows[i] = [];
+        for (let j = 0; j < letterHeight + i; j++) {
+            rows[i].push(" ");
+        }
     }
-};
 
-
-for (let i = 0; i < letterHeight; i++) {
-    rows[i].splice((letterCenter - i), 1, "*");
-    rows[i].splice((letterCenter + i), 1, "*");
-};
-
-
-for (let i = 0; i < centerRow; i ++) {
-    rows[centerRow][letterCenter - i] = "*";
-    rows[centerRow][letterCenter + i] = "*";
-};
-
-for (let i = 0; i < rows.length; i++) {
-    console.log(rows[i].join(""))
-};
+    for (let i = 0; i < letterHeight; i++) {
+        rows[i].splice((letterCenter - i), 1, "*");
+        rows[i].splice((letterCenter + i), 1, "*");
+    }
+    
+    for (let i = 0; i < centerRow; i++) {
+        rows[centerRow][letterCenter - i] = "*";
+        rows[centerRow][letterCenter + i] = "*";
+    }
+    
+    for (let i = 0; i < rows.length; i++) {
+        console.log(rows[i].join(""));
+    }
+    
+}
