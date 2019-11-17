3ee3549ab98227ef69c2215a6b7a628d5f395da2
Raziel <raziel5746@gmail.com>
2019-11-17 18:07:11 +0200

    update 17/11/19

---
+++ DAYS/Day_16/exercises.py
@@ -0,0 +1,60 @@
+# Exercise 1
+# print("Hello world\n"*4)
+
+
+# Exercise 2
+# print(99**3)*8
+
+
+# Exercise 3
+
+# name = 'Raziel'
+# age = 33
+# shoe_size = 41
+# info = "I'm {}, {} years old, and I leave {} size footprints on snow".format(name, age, shoe_size)
+
+# print(info)
+
+
+# Exercise 4
+
+# my_age = 33
+
+# future_years = 123879
+
+# age_in_123879_years = my_age + future_years
+
+# print("In {} you'll be {} years old".format(future_years, age_in_123879_years))
+
+
+# Exercise 5
+
+# first_name = "Raziel"
+# last_name = "Diaz Elsztain"
+
+# print(first_name + " " + last_name)
+
+
+# Exercise 6
+
+# computer_brand = "HP"
+
+# print("I have an {pc} computer".format(pc=computer_brand))
+
+
+# Golden Exercises
+# Exercise 1 -- DONE
+
+# Execise 2 -- DONE
+
+# Exercise 3
+
+# print("Hello World\n"*4 + "I love Python\n"*4)
+
+
+# Exercise 4
+
+# x = input("enter an integer:")
+# result = int(x) + int(x+x) + int(x+x+x) + int(x+x+x+x)
+
+# print(result)
---
+++ DAYS/Day_16/some_file.py
@@ -0,0 +1,35 @@
+name = "Raziel"
+age = 33
+
+# print("Your name is {}, and you are {age} years old".format(name, age=age))
+
+# print(f"Your name is {name}, and you are {age} years old")
+
+# print("Your name is {}, and you are {} years old".format(name, age))
+
+# print("Your name is {name}, and you are {age} years old".format(name=name, age=age))
+
+# print("Your name is %s, and you are %d years old" % (name, age))
+
+
+# -------------------------------------------------------------------------------------------
+
+# alpha = "abcdefghijklmnopqrstuvwxyz"
+
+# print(alpha[3:10]) #"defghij"
+
+# print(alpha[3:]) #"defghijklmnopqrstuvwxyz"
+
+# print(alpha[:10]) #"abcdefghij"
+
+# print(alpha[-1]) # "z"
+
+# print(alpha[::2]) # "acegikmoqsuwy"
+
+# print(alpha[::-1]) # "zyxwvutsrqponmlkjihgfedcba"c
+
+numbers = "123456789"
+
+# print(numbers[1::2]) # "2468"
+
+print(numbers[::-3])
